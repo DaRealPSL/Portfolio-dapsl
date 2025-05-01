31fbee172ce49f9dbb00dc55b605483dda68b7d7
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2025-05-01 13:11:57 +0200

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,8 @@ import { useInView } from "react-intersection-observer";
 import validator from "validator";
 
 import "../styles/main.css";
-// import handler from "../../api/send-email.cjs"
+
+import emailjs from "@emailjs/browser";
 
 const containsOffensiveTerms = (input: string, offensiveList: string[]) => {
   return offensiveList.some((term) => input.toLowerCase().includes(term));
@@ -69,10 +70,8 @@ const Contact: React.FC = () => {
       return;
     }
 
-    // Load terms from list
     const offensiveList = offensiveTerms.map((term) => term.trim());
 
-    // Check for offensive words in name and email
     if (
       containsOffensiveTerms(formData.name, offensiveList) ||
       containsOffensiveTerms(formData.email.toLowerCase(), offensiveList)
@@ -98,30 +97,25 @@ const Contact: React.FC = () => {
       return;
     }
 
-    // Indicate waiting for the server
     setStatus({ message: "Waiting for server response...", success: false });
 
     try {
-      const res = await fetch("/api/send-email", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          name: formData.name,
-          email: formData.email,
+      const res = await emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: formData.name,
+          from_email: formData.email,
           message: formData.message,
-        }),
-      });
-
-      const data = await res.json();
-      if (data.success) {
-        setStatus({ message: "Email sent successfully!", success: true });
-        setFormData({ name: "", email: "", message: "", honeypot: "" });
-      } else {
-        setStatus({ message: "Failed to send email.", success: false });
-      }
-    } catch (error) {
-      console.error("Error sending email", error);
-      setStatus({ message: "Something went wrong! Cannot submit form.", success: false });
+        },
+        import.meta.env.VITE_EMAILJS_USER_ID
+      );
+      console.log(res.text);
+      setStatus({ message: "Email sent successfully!", success: true });
+      setFormData({ name: "", email: "", message: "", honeypot: "" });
+    } catch (e) {
+      console.error("EmailJS error:", e);
+      setStatus({ message: "Failed to send email.", success: false });
     }
   };
 
